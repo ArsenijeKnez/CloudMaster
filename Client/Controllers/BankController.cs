dc010de43e970ddb004853521ea6091dc272312d
ArsenijeKnez <arsenije.knezevic6@gmail.com>
2024-10-27 09:15:50 +0100

    Bank mvc and temp data

--- Client/Controllers/BankController.cs
+++ Client/Controllers/BankController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Client.Models;
 
 namespace Client.Controllers
 {
@@ -10,7 +11,45 @@ namespace Client.Controllers
     {
         public IActionResult Index()
         {
+            return View(_clients);
+        }
+
+        private static List<BankClient> _clients = new List<BankClient> 
+        {
+            new BankClient { Id = 1, Name = "John Doe", Email = "john@example.com" },
+            new BankClient { Id = 2, Name = "Jane Smith", Email = "jane@example.com" }
+        };
+
+        private static List<MoneyTransfer> _transfers = new List<MoneyTransfer>();
+
+
+        [HttpGet]
+        public IActionResult EnlistMoneyTransfer()
+        {
+            ViewBag.Clients = _clients; 
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnlistMoneyTransfer(int fromClientId, int toClientId, double amount)
+        {
+            if (fromClientId == toClientId || amount <= 0)
+            {
+                ModelState.AddModelError("", "Invalid transfer");
+                ViewBag.Clients = _clients;
+                return View();
+            }
+
+            var transfer = new MoneyTransfer
+            {
+                Id = _transfers.Count + 1,
+                FromClientId = fromClientId,
+                ToClientId = toClientId,
+                Amount = amount,
+            };
+
+            _transfers.Add(transfer);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Client/Models/MoneyTransfer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class MoneyTransfer
+    {
+        public int Id { get; set; }
+        public int FromClientId { get; set; }
+        public int ToClientId { get; set; }
+        public double Amount { get; set; }
+    }
+}

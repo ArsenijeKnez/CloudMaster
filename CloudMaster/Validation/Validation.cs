277984ab140d582d825a179cc845904fbd99ba00
ArsenijeKnez <arsenije.knezevic6@gmail.com>
2024-10-27 08:40:27 +0100

    Added common project and IValidation

--- CloudMaster/Validation/Validation.cs
+++ CloudMaster/Validation/Validation.cs
@@ -6,46 +6,109 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
+using Common.Interfaces;
 
 namespace Validation
 {
     /// <summary>
     /// An instance of this class is created for each service instance by the Service Fabric runtime.
     /// </summary>
-    internal sealed class Validation : StatelessService
+
+    internal sealed class Validation : StatelessService, IValidation
     {
+
         public Validation(StatelessServiceContext context)
             : base(context)
         { }
 
-        /// <summary>
-        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
-        /// </summary>
-        /// <returns>A collection of listeners.</returns>
-        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        #region IValidationImplementation
+
+        public async Task<List<string>> ListAvailableItems()
         {
-            return new ServiceInstanceListener[0];
+    
+            try
+            {
+                return null;
+            }
+            catch (Exception e)
+            {
+                return null!;
+            }
         }
 
-        /// <summary>
-        /// This is the main entry point for your service instance.
-        /// </summary>
-        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
-        protected override async Task RunAsync(CancellationToken cancellationToken)
+        public async Task<string> EnlistPurchase(long? bookId, uint? count)
         {
-            // TODO: Replace the following sample code with your own logic 
-            //       or remove this RunAsync override if it's not needed in your service.
-
-            long iterations = 0;
-
-            while (true)
+            if (bookId is null || count is null)
             {
-                cancellationToken.ThrowIfCancellationRequested();
-
-                ServiceEventSource.Current.ServiceMessage(this.Context, "Working-{0}", ++iterations);
-
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+                return null!;
             }
+
+            try
+            {
+                return null;
+            }
+            catch (Exception e)
+            {
+                return null!;
+            }
+        }
+
+        public async Task<string> GetItemPrice(long? bookId)
+        {
+            if (bookId is null)
+            {
+                return null!;
+            }
+
+            try
+            {
+                return null;
+            }
+            catch (Exception e)
+            {
+                return null!;
+            }
+        }
+
+        public async Task<List<string>> ListClients()
+        {
+            try
+            {
+                //return await bankProxy.ListClients();
+                return null;
+
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public async Task<string> EnlistMoneyTransfer(long? userSend, long? userReceive, double? amount)
+        {
+            if (userSend is null || userReceive is null || amount is null)
+            {
+                return null;
+            }
+
+
+            try
+            {
+                return null;
+            }
+            catch (Exception e)
+            {
+                return null!;
+            }
+        }
+
+        #endregion
+
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return this.CreateServiceRemotingInstanceListeners();
         }
     }
 }
---
+++ Common/Interfaces/IValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+namespace Common.Interfaces
+{
+    public interface IValidation : IService
+    {
+        Task<List<string>> ListAvailableItems();
+
+        Task<string> EnlistPurchase(long? bookId, uint? count);
+
+        Task<string> GetItemPrice(long? bookId);
+
+        Task<List<string>> ListClients();
+
+        Task<string> EnlistMoneyTransfer(long? userSend, long? userReceive, double? amount);
+    }
+}
